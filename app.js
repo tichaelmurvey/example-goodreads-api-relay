5beb275dfbad865ce73c5a73a62f49c876a5a4e3
Michael Turvey <michaelturvey97@gmail.com>
2023-03-11 15:25:06 -0500

    udpate API with promise request

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const port = 3000;
 //app.use(limiter);
 app.use(cors());
 app.get("/", (req, res) => res.send("Hello World!"));
+const latDecimalConversion = 111000; //Distance between degrees of latitude
 
 // Our Goodreads relay route!
 app.get("/api/search", async (req, res) => {
@@ -38,4 +39,65 @@ app.get("/api/search", async (req, res) => {
   }
 });
 
+app.get("/api/grid", async (req, res) => {
+  try {
+    let fetchList = [];
+    elevationGrid = [];
+    const originalCoords = `${req.query.originalcoords}`.split(",");
+    const distance = `${req.query.distance}`;
+    const numPoints = `${req.query.lod}`;
+
+    lineOffset = distance/numPoints; //Distance between intervals for the purpose of graphing
+    totalLatDegrees = distance/latDecimalConversion; //Total degrees of latitude within the grid
+    latDegreeInterval = totalLatDegrees/numPoints; //Number of degrees of lattitude between fetch points
+
+    for(let i=0; i<numPoints; i++){
+      let latToGet = Number(originalCoords[0]) + totalLatDegrees/2 - latDegreeInterval*i;
+      let longDecimalConversion = longitudeDistance(latToGet); //Distance between degrees of longitude at the given latitude
+      let totalLongDegrees = distance/longDecimalConversion;
+      let longDegreeInterval = totalLongDegrees/numPoints;
+      coordset = [];
+      for(let j=0; j<numPoints; j++){
+          let newCoord = [0,0];
+          newCoord[0] = latToGet;
+          newCoord[1] = originalCoords[1] - totalLongDegrees/2 + longDegreeInterval*j;
+          coordset.push(newCoord.join(","));
+      }
+      let latString = coordset.join("|");
+      console.log("latstring", latString);
+      // const line = await fetch(
+      //   `${process.env.TOPO_URL}${latString}`
+      // )
+      fetchList.push(latString);
+    }
+    console.log("getting " + fetchList.length + " lines");
+    const response = await Promise.all(fetchList.map(line=>fetch(line))).then(function(responses){
+      return Promise.all(responses.map(function(response){
+          return response.json();
+      }))
+      }).catch(function(error){
+      console.log(error);
+    })
+
+    // const response = await fetch(
+    //   `${process.env.TOPO_URL}${searchString}`
+    // );
+    return res.json({
+      results: response
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+function longitudeDistance(latitude){
+  var latitudeRadians = latitude * Math.PI / 180;
+  return ((Math.PI/180) *6368000*Math.cos(latitudeRadians));
+}
+
+
 app.listen(port, () => console.log(`listening on port ${port}!`));
+
